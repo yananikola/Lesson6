38025b84bed417c389cef3b99e4cbc37bb44da76
Yana Nikolaeva <Nook10@inbox.ru>
2024-02-24 10:34:58 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿void RangeOfDigit(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m);
+        if (m < n)
+        {
+            Console.Write(", ");
+        }
+        RangeOfDigit(m + 1, n);
+        return;
+    }
+}
+int m = 1;
+int n = 5;
+RangeOfDigit(m, n);
