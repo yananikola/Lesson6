ac313fd4d5cc4c2e61cb7e544ff68b7e92e2b53c
Yana Nikolaeva <Nook10@inbox.ru>
2024-02-24 10:49:24 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n));
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+
+}
+int m = 3;
+int n = 2;
+AkkermanFunction(m,n);
