38aae3178f768f8c03d18f6e958f08b9aa553018
Yana Nikolaeva <Nook10@inbox.ru>
2024-02-24 11:08:23 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintArrayReversed(int[] array,int index)
+{
+if (index >= 0)
+    {
+        Console.Write(array[index] + " ");
+        PrintArrayReversed(array, index - 1);
+    }
+}
+int[] array = { 1, 2, 5, 0, 10, 34 };
+PrintArrayReversed(array, array.Length - 1);
